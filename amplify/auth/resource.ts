0971d568a074b4e4b473c21f79a06c09f359dbf2
CA\yihao <yihao@deloitte.ca>
2025-03-14 01:12:45 -0400

    test oidc

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,30 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        { 
+          name: 'AWSAmplify-POC2',
+          clientId: secret('b0be9f1b-4f41-4edd-8f1d-b31e76f3b8e0'),
+          clientSecret: secret('SAo8Q~Sd.6OfL1ZeVxAgBOUt6KawZBdGKftskbba'),
+          issuerUrl: 'https://login.microsoftonline.com/5a87a550-9504-4736-a71b-32946e3d4313/v2.0',
+
+          scopes: ['aws.cognito.signin.user.admin', 'email', 'openid', 'phone', 'profile'],          
+          attributeMapping: {
+            email: 'email'
+        }
+        
+
+        }
+      ],
+      logoutUrls: ['http://localhost:4200/', 'https://main.dlm9age1z99sy.amplifyapp.com/'],
+      callbackUrls: [
+        'http://localhost:4200/profile',
+        'https://main.dlm9age1z99sy.amplifyapp.com/profile',
+      ],
+
+    },
   },
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+import { signInWithRedirect } from 'aws-amplify/auth';
+
 
 Amplify.configure(outputs);
 
@@ -11,8 +14,36 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
 })
-export class AppComponent {
+// export class AppComponent  implements OnInit{
+  export class AppComponent{
   title = 'amplify-angular-template';
+  
+  // ngOnInit(): void {
+  //   signInWithRedirect({
+
+  //     provider: {
+  //       custom: 'AWSAmplify-POC2'
+  //     }
+  //   });
+  // }
+  test() {
+   console.log ('I am in test');
+       signInWithRedirect({
+
+      provider: {
+        custom: 'AWSAmplify-POC2'
+      }
+    });
+   console.log ('I left test');
+
+  }
+
+  // constructor(public authenticator: AuthenticatorService) {
+    constructor() {    
+    Amplify.configure(outputs);
+
+  }
+
 }
